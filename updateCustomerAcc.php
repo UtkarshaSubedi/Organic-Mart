32d8008dd750c87ce2c092995ef203bf1cee7073
Utkarsha Subedi <utkarsha.subedi@gmail.com>
2024-01-12 22:06:42 +0545

    modified

--- updateCustomerAcc.php
+++ updateCustomerAcc.php
@@ -37,7 +37,39 @@
     $row = mysqli_fetch_array($result);
 
 
+    ?>
+    <div class="forms">
+        <div class="signup">
+            <h3>Add A Product:</h3>
+            <form action="" method="POST" enctype="multipart/form-data">
+                <input type="text" name="firstname" id="username" placeholder="Product Name" value="<?php echo $row['FIRSTNAME'];?>">
+                <br>
+                <?php if (isset($nameErr)) {
+                    echo "<p> $nameErr </p>";
+                } ?>
+                <br><br>
+                <input name="email" id="username" value="<?php echo $row['EMAIL']; ?>">
+                <br>
+                <?php if (isset($desErr)) {
+                    echo "<p> $desErr </p>";
+                } ?>
+                <br><br>
 
+                <textarea rows="4" cols="50" name="username" id="username" value="<?php echo $row['USERNAME']; ?>">Allergy Information</textarea>
+                <br>
+                <?php if (isset($allergyErr)) {
+                    echo "<p> $allergyErr </p>";
+                } ?>
+                <br><br>
+                <input type="submit" name="Add" value="Add" placeholder="Add" id="signup">
+            </form>
+            <br>
+            <a style="margin-top:60px;" href="Trader.php">Go Back</a>
+        </div>
+
+    </div>
+
+    <?php include 'footer.php'; ?>
 </body>
 
 </html>
